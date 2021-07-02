08d9fbc79eacf2f689cb31719ad6384ade78af5c
hy3592 <hy3592@likelion.org>
2021-07-02 15:09:43 +0900

    step step

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
-def hi_nini (request):
-    return render(request, 'accountapp/hi_nini.html')
+def hi_nini(request):
+    return render(request, 'accountapp')
