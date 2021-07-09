78039040e207ebdbcd25da8adc533720ae3cbe09
hy3592 <hy3592@likelion.org>
2021-07-09 12:53:34 +0900

    7.9 start 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,10 +3,21 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import NewModel
+
+
 def hi_nini(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+
         return render(request, 'accountapp/hi_nini.html',
-                      context={'text':'POST METHOD'})
+                      context={'new_model':new_model})
     else:
         return render(request, 'accountapp/hi_nini.html',
                       context={'text':'GET METHOD'})
