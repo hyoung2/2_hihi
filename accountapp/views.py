4c3d3520ba4cff4ff45cff22f052f30e382a77d3
hy3592 <hy3592@likelion.org>
2021-07-09 12:06:31 +0900

    7.9 start 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def hi_nini(request):
-    return render(request, 'accountapp/hi_nini.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hi_nini.html',
+                      context={'text':'POST METHOD'})
+    else:
+        return render(request, 'accountapp/hi_nini.html',
+                      context={'text':'GET METHOD'})
+
