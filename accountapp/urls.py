8fcf371bc43d8e859290d47e21937ae007ac6732
hy3592 <hy3592@likelion.org>
2021-07-13 12:40:21 +0900

    7.13 start 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from accountapp.views import hi_nini
+from accountapp.views import hi_nini, AccountCreateView
 
 app_name = 'accountapp'
 
-urlpatterns = [
-    path('hi_nini/', hi_nini, name='aloha')
-]
+urlpatterns = (
+    path('hi_nini/', hi_nini, name='aloha'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
+)
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -24,3 +27,11 @@ def hi_nini(request):
         return render(request, 'accountapp/hi_nini.html',
                       context={'data_list': data_list})
 
+
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:aloha')
+    templates_name = 'accountapp/create.html'
