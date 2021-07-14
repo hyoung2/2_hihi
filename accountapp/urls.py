66f1718bde7f4b257d677ae554d573982086f123
hy3592 <hy3592@likelion.org>
2021-07-14 23:32:19 +0900

    7.14 start 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from accountapp.views import hi_nini, AccountCreateView
@@ -7,6 +8,10 @@ app_name = 'accountapp'
 
 urlpatterns = (
     path('hi_nini/', hi_nini, name='aloha'),
+
+    path('login/',LoginView.as_view(template_name = 'accountapp/login.html'),name = 'login'),
+
+    path('logout/',LogoutView.as_view(),name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create')
 )
